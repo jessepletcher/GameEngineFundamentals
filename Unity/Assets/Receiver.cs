a483c74f0ca121c3bfa08110e1b9356f912f7fad
JBugga <114814710+jessepletcher@users.noreply.github.com>
2026-02-17 18:38:38 -0700

    U

---
+++ Unity/Assets/Receiver.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnCalled()
+    {
+        print("Call Received");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
